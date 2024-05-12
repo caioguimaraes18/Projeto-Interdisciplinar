930c769ed8cbfa3c90b0252f4a4a9bdb91a7cac6
Caio Guimarães <contatocaioguimaraess@gmail.com>
2024-05-12 13:53:59 -0300

    Fix: Projeto PI

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # Autores: Caio Guimarães RGM: 31319343
-#        Giovanni Santos RGM: 631660754
-#        Otávio Silva RGM: 31732330
+#          Giovanni Santos RGM: 631660754
+#          Otávio Silva RGM: 31732330
 
 # Professor: Giulio Guiyti Rossignolo Suzumura & Antonio Eduardo Marques Da Silva
 
 
-#Funções de conversão de bases numéricas
+# Funções de conversão de bases numéricas
+
+import time
+
 
 def bin_to_dec(num_binario):
     return int(num_binario, 2)
@@ -33,10 +36,10 @@ while opcao1 != 3:
     
     escolha = input("Escolha uma opção: ")
    
-   #Vamos fazer a cibversão de decimal para binário, hexadecimal e octal 
+    # Vamos fazer a conversão de decimal para binário, hexadecimal e octal 
     if escolha == "1":
         while True:
-            num_decimal = int(input("Digite o número decimal: "))
+            num_decimal = input("Digite o número decimal: ")
             # Verifica se todos os caracteres são dígitos
             if all (digito in ['0','1','2','3','4','5','6','7','8','9'] for digito in str(num_decimal)):
                 break
@@ -44,17 +47,19 @@ while opcao1 != 3:
                 print("Número decimal inválido. Por favor, digite apenas números.")
         
         #
-        decimal_to_binary = bin(num_decimal)[2:]  # Removendo o prefixo '0b'
-        decimal_to_hexadecimal = hex(num_decimal)[2:]  # Removendo o prefixo '0x'
-        decimal_to_octal = oct(num_decimal)[2:] # Removendo o prefixo '0o'    
-        
-        # Exibe os resultados
+        decimal_to_binary = bin(int(num_decimal))[2:]  # Removendo o prefixo '0b'
+        decimal_to_hexadecimal = hex(int(num_decimal))[2:]  # Removendo o prefixo '0x'
+        decimal_to_octal = oct(int(num_decimal))[2:] # Removendo o prefixo '0o'    
+        print("Processando...")
+        time.sleep(1)
+        # Exibe os resultados1
+    
         print(f"Resultado Binário: {decimal_to_binary}")
         print(f"Resultado Hexadecimal: {decimal_to_hexadecimal}")
         print(f"Resultado Octal: {decimal_to_octal}")
     
     
-    #Vamos fazer a conversão de binário, hexadecimal e octal para decimal    
+    # Vamos fazer a conversão de binário, hexadecimal e octal para decimal    
     if escolha == "2":
         while True:
             num_binario = input("Digite o número binário: ")
@@ -82,7 +87,9 @@ while opcao1 != 3:
                 break
             else:
                 print("Número octal inválido. Por favor, digite apenas '0' a '7'.")
-           
+        
+        print("Processando...")
+        time.sleep(1)   
         # Converte e exibe os resultados
         print(f"Conversão de binários para decimal: {bin_to_dec(num_binario)}")
         print(f"Conversão de hexadecimal para decimal: {hex_to_dec(num_hex)}")
